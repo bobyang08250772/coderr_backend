08e27425167e7f1ce2ee51cdfa7d001ebf730cb2
bobyang08250772 <87290044+bobyang08250772@users.noreply.github.com>
2025-07-24 10:55:28 +0200

    Docker added

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Urls
 urlpatterns = [
@@ -28,4 +29,4 @@ urlpatterns = [
 
 # Medial url
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL)
+urlpatterns += staticfiles_urlpatterns
